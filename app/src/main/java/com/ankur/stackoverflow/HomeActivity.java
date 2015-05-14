64c0414b590c5f5cf094e834cf599739ecb779ac
Ankur Jain <ankur@bsb.in>
2015-05-14 22:06:43 +0530

    cleanup

--- app/src/main/java/com/ankur/stackoverflow/HomeActivity.java
+++ app/src/main/java/com/ankur/stackoverflow/HomeActivity.java
@@ -1,16 +1,9 @@
 package com.ankur.stackoverflow;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
 
 public class HomeActivity extends ActionBarActivity {
 
@@ -19,9 +12,6 @@ public class HomeActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
         }
     }
 
@@ -48,19 +38,4 @@ public class HomeActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            return rootView;
-        }
-    }
 }
