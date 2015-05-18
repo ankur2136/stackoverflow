300499e4cb2d23ded01c10047d463da18261864c
Ankur Jain <ankur@bsb.in>
2015-05-18 23:12:07 +0530

    removed job queue

--- app/src/main/java/com/ankur/stackoverflow/MyApplication.java
+++ app/src/main/java/com/ankur/stackoverflow/MyApplication.java
@@ -1,18 +1,11 @@
 package com.ankur.stackoverflow;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import android.app.Application;
 
-import com.ankur.stackoverflow.jobs.JobManagerLogger;
 import com.ankur.stackoverflow.utils.NetworkConnectivityListener;
 import com.ankur.stackoverflow.utils.NetworkUtils;
 import com.ankur.stackoverflow.utils.SharedPrefs;
 import com.ankur.stackoverflow.utils.VolleyLib;
-import com.path.android.jobqueue.JobManager;
-
-import com.path.android.jobqueue.config.Configuration;
 
 public class MyApplication extends Application implements NetworkConnectivityListener.ConnectivityChangedListener {
 
@@ -24,17 +17,9 @@ public class MyApplication extends Application implements NetworkConnectivityLis
 
     public static final int      NETWORK_TYPE_CELLULAR    = 1;
 
-    private static Set<String>   sDialogsOnScreen;
-
-    private static boolean       sPlayerExpanded          = false;
-
     private static int           sNetworkType             = -1;
 
     private static int           sNetworkSubType          = -1;
-
-    private boolean              mHideNetworkNotification = false;
-
-    private static JobManager    mPersistentJobManager;
 
     public MyApplication() {
         mMyApplication = this;
@@ -63,35 +48,8 @@ public class MyApplication extends Application implements NetworkConnectivityLis
     @Override
     public void onCreate() {
         super.onCreate();
-        sDialogsOnScreen = new HashSet<>();
         VolleyLib.init(this);
         SharedPrefs.getInstance().init(this);
-        configureJobManagers();
-    }
-
-    /*
-     * @Returns true if set is modified. false otherwise.
-     */
-    public boolean addDialogToSet(String id) {
-        return sDialogsOnScreen.add(id);
-    }
-
-    public boolean removeDialogFromSet(String id) {
-        return sDialogsOnScreen.remove(id);
-    }
-
-    private void configureJobManagers() {
-        Configuration persistentJobManager = new Configuration.Builder(this).customLogger(new JobManagerLogger())
-                .minConsumerCount(1)// always keep at least one consumer alive
-                .maxConsumerCount(3)// up to 3 consumers at a time
-                .loadFactor(3)// 3 jobs per consumer
-                .consumerKeepAlive(120)// wait 2 minute
-                .build();
-        mPersistentJobManager = new JobManager(this, persistentJobManager);
-    }
-
-    public static JobManager getJobManager() {
-        return mPersistentJobManager;
     }
 
     @Override
@@ -104,21 +62,5 @@ public class MyApplication extends Application implements NetworkConnectivityLis
             }
             MyApplication.setNetworkSubType(NetworkUtils.getNetworkType(this));
         }
-    }
-
-    public boolean hideNetworkNotification() {
-        return mHideNetworkNotification;
-    }
-
-    public void setNetworkNotificationVisibility(boolean hideNotification) {
-        this.mHideNetworkNotification = hideNotification;
-    }
-
-    public void setConfigSyncedExpiry() {
-        SharedPrefs.getInstance().setSyncedUserStateExpirationTime(System.currentTimeMillis() + (1000 * 1800));
-    }
-
-    public static boolean isConfigSyncedExpired() {
-        return System.currentTimeMillis() >= SharedPrefs.getInstance().getSyncedUserStateExpirationTime();
     }
 }
